a667e89c38c370797feb7ff082446012a861d347
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-04-09 13:05:18 -0700

    created boilerplate server

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require(`express`);
+const cors = require(`cors`);
+
+// Port environment variables
+const PORT = process.env.PORT || 9999;
+
+// Server instance
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Lovely greeting for anyone who tries to visit the deployed server
+app.get(`/`, (req, res) => {
+    res.send(`I didn't hire you to stand around and spray, now get to work!`)
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+})
